694a2ef65a49067a394548a5619e41c844c01dc4
Darren Wang <rardenawng@gmail.com>
2018-06-29 15:25:59 -0400

    Update MultipleXDataset_LinReg_Gradient_Descent.java

--- MultipleXDataset_LinReg_Gradient_Descent.java
+++ MultipleXDataset_LinReg_Gradient_Descent.java
@@ -5,12 +5,19 @@ import java.util.Scanner;
 public class MultipleXDataset_LinReg_Gradient_Descent {
 
 	public static void main(String[] args) {
+
+		// Initializes the input Scanner
 		Scanner input = new Scanner(System.in);
+
 		int num_of_x = -1;
-
+		
+		//Conditioning for adding the input data points into array lists
 		boolean invalid = true;
+		
+		//2D array list that stores all of the data points (or training sets)
 		List<List<Double>> master_arr = new ArrayList<List<Double>>();
-
+		
+		//Turns the input strings into data points inside the array lists
 		while (invalid) {
 			System.out.println("Number of X categories: ");
 			try {
@@ -45,23 +52,29 @@ public class MultipleXDataset_LinReg_Gradient_Descent {
 			}
 			System.out.println(master_arr.toString());
 		}
-		
+
 		ArrayList<Double> cost_sum = new ArrayList<Double>();
 		ArrayList<Double> thetas = new ArrayList<Double>();
 		thetas = fillZero(thetas, num_of_x + 1);
 
 		int training_sets = master_arr.get(0).size();
 		double cost = 0.0;
-		double learning_rate = 0.01;
+		double learning_rate = 0.025;
 		double total_cost = 0.0;
-
-		for (int i = 0; i < 10000; i++) {
+		
+		/*
+		 * Using the cost function, partial derivatives, and LMS algorithm to find the best line of fit
+		 * The first outer loop controls the iterations
+		 */
+		
+		for (int i = 0; i < 1000; i++) {
 			cost_sum = fillZero(cost_sum, num_of_x + 1);
+			//Calculates the cost for each X data sets
 			for (int j = 0; j < training_sets; j++) {
-				cost = linear_regression(master_arr, thetas, j) - master_arr.get(master_arr.size()-1).get(j);
+				cost = linear_regression(master_arr, thetas, j) - master_arr.get(master_arr.size() - 1).get(j);
 				cost_sum.set(0, cost_sum.get(0) + cost);
 				for (int c = 1; c < cost_sum.size(); c++) {
-					cost_sum.set(c, cost_sum.get(c) + cost * master_arr.get(c-1).get(j));
+					cost_sum.set(c, cost_sum.get(c) + cost * master_arr.get(c - 1).get(j));
 				}
 			}
 
@@ -70,18 +83,22 @@ public class MultipleXDataset_LinReg_Gradient_Descent {
 				thetas.set(j, thetas.get(j) - learning_rate * cost_sum.get(j));
 			}
 		}
+		
+		//Rounding to the nearest thousandth place
 		for (int i = 0; i < thetas.size(); i++) {
 			double round_theta = Math.round(thetas.get(i) * 1000.0) / 1000.0;
 			thetas.set(i, round_theta);
 		}
-		String str = "y = " + thetas.get(0)  + " + ";
+		
+		//Creating the final equation string
+		String str = "y = " + thetas.get(0) + " + ";
 		for (int i = 1; i < thetas.size(); i++) {
 			str += "" + thetas.get(i) + "x" + i + " + ";
 		}
-		System.out.println(str.substring(0,str.length()-2));
+		System.out.println(str.substring(0, str.length() - 2));
 
 	}
-
+	
 	private static ArrayList<Double> seperate(String str, ArrayList<Double> arr) {
 		if (str.length() < 1) {
 			return arr;
@@ -97,7 +114,7 @@ public class MultipleXDataset_LinReg_Gradient_Descent {
 			return seperate(str, arr);
 		}
 	}
-
+	//Fills the array with all zeros with the given size
 	private static ArrayList<Double> fillZero(ArrayList<Double> arr, int size) {
 		arr = new ArrayList<Double>();
 		for (int i = 0; i < size; i++) {
@@ -105,11 +122,11 @@ public class MultipleXDataset_LinReg_Gradient_Descent {
 		}
 		return arr;
 	}
-	
+	//Applies the linear regression equation to get the predicted values for the program to learn from
 	private static double linear_regression(List<List<Double>> xval, ArrayList<Double> thetas, int index) {
 		double sum = thetas.get(0);
 		for (int i = 1; i < xval.size(); i++) {
-			sum += xval.get(i-1).get(index) * thetas.get(i);
+			sum += xval.get(i - 1).get(index) * thetas.get(i);
 		}
 		return sum;
 	}
